1ada7707fc3c874af3dcdc36c91446ce2830e709
Sindre Bøyum <sindre.boyum@tietoevry.com>
2022-04-18 10:40:05 +0200

    typeify code

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const input = document.querySelector("input");
 const resultEl = document.querySelector(".result");
 
+if (!input || !resultEl) {
+  throw new Error("Could not find 'input' or '.result' elements.");
+}
+
 input.addEventListener("input", () => {
   input.value = cleanValue(input.value);
 
@@ -9,17 +13,29 @@ input.addEventListener("input", () => {
     shortenedWords.length > 0 ? shortenedWords.join(" ") : "";
 });
 
+/**
+ * @param {string} str
+ * @returns {string}
+ */
 function cleanValue(str) {
   return str
     .replace(/[|"'_\.\,\-\#\!\¤\%\&\/\`\(\)\\\=\?\+\{\}\~\¨\^\*]/gi, "")
     .replace(/\s{2,}/gi, " ");
 }
 
+/**
+ * @param {string} wordsString
+ * @returns {Array<string>}
+ */
 function shortenWords(wordsString) {
   const words = wordsString.split(" ");
   return words.map(shortenWord);
 }
 
+/**
+ * @param {string} word
+ * @returns {string}
+ */
 function shortenWord(word) {
   word = word.toLowerCase();
 
@@ -35,9 +51,10 @@ function shortenWord(word) {
 }
 
 const queryParams = new URL(location.href).searchParams;
+const queryWord = queryParams.get("word");
 
-if (queryParams.has("word")) {
-  input.value = cleanValue(queryParams.get("word"));
+if (queryWord) {
+  input.value = cleanValue(queryWord);
 
   const shortenedWords = shortenWords(input.value);
   resultEl.textContent =
